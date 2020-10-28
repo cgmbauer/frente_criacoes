75db1b7c55c8ba1b22e8e1f87d3e9ab7275708ef
cgmbauer <cgmbauer@gmail.com>
2020-10-28 09:27:35 -0300

    Initial commit

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const SignIn = () => {
+  return (
+    <>
+      <Container>
+        <h1>Hello World.</h1>
+        <p>SignIn page</p>
+      </Container>
+    </>
+  );
+};
+
+export default SignIn;
