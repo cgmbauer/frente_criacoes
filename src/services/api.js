30cf97e67c8faf4d3d8bc331d40f625b4081ef8c
Guilherme Bauer <62662494+cgmbauer@users.noreply.github.com>
2020-11-19 11:26:20 -0300

    refactor: baseUrl

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/',
-  // baseURL: 'https://remotecontroller.herokuapp.com/',
+  baseURL: 'https://remotecontroller.herokuapp.com/',
 });
 
 export default api;
